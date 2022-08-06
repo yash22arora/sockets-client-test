1fbfffa5fd816bd8d3620505612fc048b8046e39
Yashvardhan Arora <yash22arora@gmail.com>
2022-08-07 02:18:22 +0530

    Setup socket client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3001");
 
 function App() {
+  const [text, setText] = useState("");
+  const [name, setName] = useState("");
+  const [chat, setChat] = useState([]);
+  const [joined, setJoined] = useState(false);
+
+  useEffect(() => {
+    socket.emit("findAllMessages", {}, (response) => {
+      console.log(response);
+      setChat(response);
+    });
+  });
+
+  const join = (e) => {
+    e.preventDefault();
+    socket.emit("join", { name }, () => {
+      setJoined(true);
+    });
+  };
+
+  const onMessageSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("createMessage", { text }, () => {
+      setText("");
+    });
+  };
+
+  const onNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const onTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const renderChat = () => {
+    return chat.map((message, index) => {
+      return (
+        <div key={index}>
+          <p>
+            [{message.name}]: {message.text}
+          </p>
+        </div>
+      );
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ textAlign: "center" }}>Chat</h1>
+      <div className="chatLog">{renderChat()}</div>
+      <form
+        onSubmit={joined ? (e) => onMessageSubmit(e) : (e) => join(e)}
+        className="chatForm"
+      >
+        {!joined ? (
+          <>
+            <p>Enter Name:</p>
+            <input
+              name="name"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => onNameChange(e)}
+            />
+          </>
+        ) : (
+          <>
+            <input
+              name="message"
+              placeholder="Message"
+              value={text}
+              onChange={(e) => onTextChange(e)}
+            />
+          </>
+        )}
+
+        <button type="submit">Send Message</button>
+      </form>
+    </>
   );
 }
 
